3dbbc6c6e8d3e40f04ff6ec31f8e55f26eb08608
Hansel <fernandohanselsc@gmail.com>
2025-07-15 13:38:34 -0300

    feat: cria ProdutoRepository com JpaRepository

---
+++ src/main/java/com/hansel/produto/repository/ProdutoRepository.java
@@ -0,0 +1,8 @@
+package com.hansel.produto.repository;
+
+import com.hansel.produto.model.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+
+}
