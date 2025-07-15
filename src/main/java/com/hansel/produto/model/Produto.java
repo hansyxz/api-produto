d108d54c50a68f6155e68d8ced693f4a1a0dc391
Hansel <fernandohanselsc@gmail.com>
2025-07-15 17:34:59 -0300

    chore: remove Lombok do projeto e adiciona getters/setters manuais

--- src/main/java/com/hansel/produto/model/Produto.java
+++ src/main/java/com/hansel/produto/model/Produto.java
@@ -1,17 +1,12 @@
 package com.hansel.produto.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
 @Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class Produto {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -23,4 +18,36 @@ public class Produto {
 
     @Column(nullable = false)
     private BigDecimal preco;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
 }
