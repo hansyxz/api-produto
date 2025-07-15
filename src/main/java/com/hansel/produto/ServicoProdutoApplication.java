7ece222279f725009f229eddc1b18005a7ddee2f
Hansel <fernandohanselsc@gmail.com>
2025-07-15 13:15:56 -0300

    feat: cria entidade Produto com campos básicos

---
+++ src/main/java/com/hansel/produto/ServicoProdutoApplication.java
@@ -0,0 +1,13 @@
+package com.hansel.produto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicoProdutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicoProdutoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/hansel/produto/model/Produto.java
@@ -0,0 +1,26 @@
+package com.hansel.produto.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    private String descrição;
+
+    @Column(nullable = false)
+    private BigDecimal preco;
+}
